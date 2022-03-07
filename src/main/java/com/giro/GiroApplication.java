49b4704ebe5e4ed93ba9dba244cce475a6c1865b
Lemoel Marques <lemoel@gmail.com>
2022-03-07 13:04:03 +0000

    ajustes nos pagotes

--- src/main/java/com/giro/GiroApplication.java
+++ src/main/java/com/giro/GiroApplication.java
@@ -1,4 +1,4 @@
-package com.gito;
+package com.giro;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
--- src/main/java/com/giro/resource/ClientesResource.java
+++ src/main/java/com/giro/resource/ClientesResource.java
@@ -1,4 +1,4 @@
-package com.gito.resource;
+package com.giro.resource;
 
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
